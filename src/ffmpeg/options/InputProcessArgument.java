5366f0af9e819e6ad0ced8ff68f6a41b8a5bb19b
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2021-12-13 23:07:44 +0000

    Inheritance applied to the InputProcessArgument.java as it IS a ProcessArgument.

--- src/ffmpeg/options/InputProcessArgument.java
+++ src/ffmpeg/options/InputProcessArgument.java
@@ -4,8 +4,11 @@ package ffmpeg.options;
  * Represents when some process argument requires a value pair for example the
  * '-h' does not require any input whereas '-loglevel' requires some 'value'
  * such as '-loglevel verbose'.
+ * <p>
+ * Note that all {@link InputProcessArgument} must have a Default value handler
+ * that being the {@link ProcessArgument}.
  */
-public interface InputProcessArgument {
+public interface InputProcessArgument extends ProcessArgument {
 
     /**
      * Compile the process argument from the provided args string.
